5ebec0d15694ee97fa511a5fc410b4875f258d55
Yannick Pflanzer <yp1995@outlook.com>
2016-03-16 20:10:49 +0100

    + Implemented fork

--- libdayos/src/exec.c
+++ libdayos/src/exec.c
@@ -3,8 +3,15 @@
 #include <syscall.h>
 #include <message.h>
 #include <string.h>
+#include <unistd.h>
+#include <arch.h>
 
 int system(const char* command)
 {
 	return 1;
+}
+
+pid_t fork()
+{
+	syscall1(SYSCALL_FORK, 0);
 }

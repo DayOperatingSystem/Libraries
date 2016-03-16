af2c5edba814e98f0d68f672c7d9a0012d230df0
Yannick Pflanzer <yp1995@outlook.com>
2016-03-16 14:37:25 +0100

    * putenv now handles existing environment variables correctly

--- libdayos/src/environment.c
+++ libdayos/src/environment.c
@@ -14,12 +14,35 @@ char* getenv(const char* name)
 	return NULL;
 }
 
-/// FIXME: Specs say "don't copy" but should we?
-int putenv(char* name)
+static void add_variable(char* name)
 {
 	environ = (char**) realloc(environ, (environ_size + 2) * sizeof(char**));
 
 	environ[environ_size] = name;
 	environ_size++;
 	environ[environ_size] = NULL;
+}
+
+static size_t get_variable_id(char** env, size_t num, const char* name)
+{
+	const char* equals = strchr(name, '=');
+	size_t size = equals - name;
+	
+	for(size_t i = 0; i < environ_size; i++)
+		if(!strncmp(env[i], name, size))
+			return i;
+		
+	return -1;
+}
+
+/// FIXME: Specs say "don't copy" but should we?
+int putenv(char* name)
+{
+	size_t idx = get_variable_id(environ, environ_size, name);
+	if(idx == -1)
+		add_variable(name);
+	else
+		environ[idx] = name;
+	
+	return 0;
 }

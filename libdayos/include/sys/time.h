264b0c784255b3bd314b9ce42789a62c8847d694
Yannick Pflanzer <yp1995@outlook.com>
2016-03-21 11:21:50 +0100

    + Added some time related functions

--- libdayos/include/sys/time.h
+++ libdayos/include/sys/time.h
@@ -4,11 +4,22 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <sys/types.h>
+#include <kernelconfig.h>
 
 struct timeval
 {
 	time_t tv_sec;
 	suseconds_t tv_usec;
 };
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int gettimeofday(struct timeval* tp, void* tzp);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
--- libdayos/src/time.c
+++ libdayos/src/time.c
@@ -1,50 +1,68 @@
 #include <time.h>
 #include <syscall.h>
+#include <arch.h>
+#include <dayos.h>
 
 // FIXME: Implementation!
 clock_t clock()
 {
-	return 0;
+	return syscall1(SYSCALL_GET_TICKS, 0);
 }
 
 // FIXME: Implementation!
 time_t time(time_t* timer)
 {
-	return 0;
-	//return *timer;
+	time_t retval = clock() / CLOCKS_PER_SEC;
+	if(timer)
+		*timer = retval;
+
+	return retval;
 }
 
 struct tm* gmtime(const time_t* timer)
 {
+	DSTUB;
 	return NULL;
 }
 
 struct tm* localtime(const time_t* timer)
 {
+	DSTUB;
 	return NULL;
 }
 
 size_t strftime(char* ptr, size_t maxsize, const char* format, const struct tm* timeptr)
 {
+	DSTUB;
 	return 0;
 }
 
 time_t mktime(struct tm* timeptr)
 {
+	DSTUB;
 	return 0;
 }
 
 double difftime(time_t end, time_t beginning)
 {
+	DSTUB;
 	return 0;
 }
 
 char* ctime (const time_t* timer)
 {
+	DSTUB;
 	return " ";
 }
 
 char* asctime(const struct tm* timeptr)
 {
+	DSTUB;
 	return " ";
+}
+
+int gettimeofday(struct timeval* tp, void* tzp)
+{
+	tp->tv_sec = syscall1(SYSCALL_GET_TICKS, 0) / CLOCKS_PER_SEC;
+	tp->tv_usec = 0; // TODO: High precision timer!
 }
